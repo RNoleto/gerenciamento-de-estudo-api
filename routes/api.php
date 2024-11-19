f2595e2abd30a3746dc274713d543cfd18810afc
Ramon Noleto <devnoleto@gmail.com>
2024-11-19 15:16:22 -0300

    :wrench: feat: Endpoint para listagem de usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/subjects', [SubjectController::class, 'index']);
+
+Route::get('/users', [UserController::class, 'index']);
