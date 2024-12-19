9d463f5661ca3d7dd3c2b1e50eff615de472ea88
Ramon Noleto <devnoleto@gmail.com>
2024-12-19 11:02:15 -0300

    Teste json

--- app/Http/Controllers/CareerController.php
+++ app/Http/Controllers/CareerController.php
@@ -10,6 +10,19 @@ class CareerController extends Controller
 {
     public function index()
     {
-        return response()->json(Career::all());
+        // return response()->json(Career::all());
+        $arr = [
+            [
+                "carreira" => "teste json"
+            ],
+            [
+                "carreira" => "teste json"
+            ],
+            [
+                "carreira" => "teste json"
+            ]
+            ];
+            return response()->json($arr); 
+
     }
 }
