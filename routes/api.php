9e0645ff0a2252340efa930786cf5dd34e450baa
Ramon Noleto <ramon.zap@hotmail.com>
2025-01-10 20:18:20 -0300

    teste vercel

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,9 @@ use App\Http\Controllers\UserStudyRecordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 //Usuarios
 Route::get('/users', [UserController::class, 'index']);
