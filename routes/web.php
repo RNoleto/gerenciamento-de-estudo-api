d4c2f7c699e454decdb51989c26b4e66c278c8f2
Ramon Noleto <devnoleto@gmail.com>
2025-01-15 13:46:39 -0300

    Voltando rota welcome em web

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/teste', function () {
     return 'Olá rota WEB teste';
