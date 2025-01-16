eab75fa3b818c4e8a99e3794a9d51b10ca8e48d9
Ramon Noleto <ramon.zap@hotmail.com>
2025-01-16 20:30:08 -0300

    Revert "teste de CORS"
    This reverts commit 0850b0c846d87131c9cb3fe1227d18c390e1dafb.

--- config/cors.php
+++ config/cors.php
@@ -23,12 +23,12 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*',  'Authorization'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
