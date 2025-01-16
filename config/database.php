14e58cab942553a78aa3f7e1373afce9008ea94c
Ramon Noleto <ramon.zap@hotmail.com>
2025-01-15 22:05:14 -0300

    MIGRATION FUNCIONANDO - Testando rotas
    'sslmode' => 'require',

--- config/database.php
+++ config/database.php
@@ -31,7 +31,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'sslmode' => env('DB_SSLMODE', 'require'),
+            'sslmode' => 'require',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
