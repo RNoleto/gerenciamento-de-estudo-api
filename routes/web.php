67006959c34e13ae4ab9242cdb1f6c173fc87d38
Ramon Noleto <ramon.zap@hotmail.com>
2025-02-05 21:07:25 -0300

    :moneybag: fix: Ajuste rota welcome em web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/teste', function () {
     return 'Olá rota WEB teste';
 });
