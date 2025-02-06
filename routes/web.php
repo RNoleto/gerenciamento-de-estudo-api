844d2a561694036488e7f7a53fcc26c736875247
Ramon Noleto <ramon.zap@hotmail.com>
2025-02-05 21:05:11 -0300

    :moneybag: fix: Ajuste web.php

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\Stripecontroller@index')->name('index');
-Route::post('/checkout', 'App\Http\Controllers\Stripecontroller@checkout')->name('checkout');
-Route::get('/success', 'App\Http\Controllers\Stripecontroller@success')->name('success');
-
 Route::get('/teste', function () {
     return 'Olá rota WEB teste';
 });
