8fa18cc0e7c2dc77211470992c2320fe20cd4939
Ramon Noleto <devnoleto@gmail.com>
2025-04-14 22:01:45 -0300

    :tada: feat: Edição do layout do email de suporte

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,5 @@
+<tr>
+  <td class="footer">
+    <p>&copy; {{ date('Y') }} Estuday. Todos os direitos reservados.</p>
+  </td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+  <td class="header">
+    <a href="{{ config('app.url') }}" style="font-size: 20px; font-weight: bold; color: #4F46E5;">
+      <!-- <img src="https://estuday.com.br/img/metaImg.webp" alt="Estuday" height="50"> -->
+       Estuday
+    </a>
+  </td>
+</tr>
