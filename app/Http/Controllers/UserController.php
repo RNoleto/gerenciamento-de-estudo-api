8b19148809e75a2d37714e743ac696081de30b67
Ramon Noleto <devnoleto@gmail.com>
2025-08-07 17:00:40 -0300

    Listar infos de usuários para admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserCareer;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::with(['userCareer.career'])
+                     ->orderBy('created_at', 'desc')
+                     ->paginate(15);
+
         return response()->json($users);
+    }
+
+    public function userCareer()
+    {
+        return $this->hasOne(UserCareer::class, 'user_id', 'firebase_uid');
     }
 }
