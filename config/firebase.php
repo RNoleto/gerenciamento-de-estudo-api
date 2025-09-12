271615db929110bd1b195a8a6525feda0a9da75f
Ramon Noleto <devnoleto@gmail.com>
2025-09-11 21:42:17 -0300

    git commit -m "chore: Upgrade config firebase.php

--- config/firebase.php
+++ config/firebase.php
@@ -1,6 +1,4 @@
 <?php
 return [
-    'credentials' => env('FIREBASE_CREDENTIALS_JSON')
-        ? json_decode(env('FIREBASE_CREDENTIALS_JSON'), true)
-        : storage_path('app/firebase/firebase_credentials.json'),
+    'credentials' => storage_path('app/firebase/firebase_credentials.json'),
 ];
